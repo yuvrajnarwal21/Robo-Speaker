911549f6de2c8042b8264eddb17aebc7b2e51dc2
Yuvraj Narwal <94794548+yuvrajnarwal21@users.noreply.github.com>
2025-07-13 18:51:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ if __name__ == '__main__':
     print("Welcome to RoboSpeaker 1.1 Created by Yuvi")
     while True:
         x = input("Enter what you want me to speak: ")
+        if x == "q":
+            break
         command = f"say {x}"
         os.system(command)
